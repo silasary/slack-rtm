be3fc691ac7bb53c5df641f935d6de152cb604cc
Katelyn Gigante <katelyn@katelyngigante.com>
2014-12-26 20:40:28 +1100

    Ooops. IsChannel was defined twice.

--- SlackRTM/Channel.cs
+++ SlackRTM/Channel.cs
@@ -1,27 +1,17 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace SlackRTM
+﻿namespace SlackRTM
 {
     public class Channel
     {
         public string Id { get; set; }
 
-        public string Name { get; set; }
-
-        public bool IsChannel { get; set; }
-
         public bool IsArchived { get; set; }
 
-        public bool IsMember { get; set; }
+        public bool IsChannel { get; set; }
 
         public bool IsGeneral { get; set; }
 
-        public bool IsChannel { get; set; }
+        public bool IsMember { get; set; }
 
-
+        public string Name { get; set; }
     }
 }
