1c934931f1a687d4d36a6f3aae485ea8ab23d610
Katelyn Gigante <katelyn@katelyngigante.com>
2014-12-26 16:50:42 +1100

    Cleanup

--- SlackRTM/Channel.cs
+++ SlackRTM/Channel.cs
@@ -8,22 +8,19 @@ namespace SlackRTM
 {
     public class Channel
     {
-        [JsonProperty("id")]
         public string Id { get; set; }
 
-        [JsonProperty("name")]
         public string Name { get; set; }
 
-        [JsonProperty("is_channel")]
         public bool IsChannel { get; set; }
 
-        [JsonProperty("is_archived")]
         public bool IsArchived { get; set; }
 
-        [JsonProperty("is_member")]
         public bool IsMember { get; set; }
 
         public bool IsGeneral { get; set; }
+
+        public bool IsChannel { get; set; }
 
 
     }
